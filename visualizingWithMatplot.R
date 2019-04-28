62def0ab7d707414bf693670225c8eeecfc6f743
sonysasane <45103490+sonysasane@users.noreply.github.com>
2019-04-27 22:54:42 -0500

    Add files via upload

---
+++ visualizingWithMatplot.R
@@ -0,0 +1,4 @@
+FieldGoals
+t(FieldGoals)
+matplot(t(FieldGoals/FieldGoalAttempts),type = "b", pch = 15:18, col= c(1:4,7) )
+legend( "bottomleft",inset = 0.01,legend = Players,pch = 15:18, col= c(1:4,7)  )
